e1c296b3480f6fcd12a28fd371464689bf4b8d0d
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-23 20:36:41 +0100

    added font file to tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const plugin = require("tailwindcss/plugin");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["digitalMono", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "digitalMono",
+          src: "url(/src/fonts/digital-7.mono.ttf)",
+        },
+      });
+    }),
+  ],
 };
