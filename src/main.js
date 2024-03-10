72c42e4eecaf78045dfe66f2151f534b9479fe8a
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-10 19:43:00 +0100

    loop and randomNumber function with element id

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-// if number has less that 6 digits, add leading zeros
-const randomNumber = () => {
-  document.getElementById("1").innerHTML = Math.floor(Math.random() * 999999)
-    .toString()
-    .padStart(6, "0");
-};
+function randomNumber(id) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.innerHTML = Math.floor(Math.random() * 999999)
+      .toString()
+      .padStart(6, "0");
+  }
+}
+
+function loop(number) {
+  for (let i = 1; i <= number; i++) {
+    randomNumber(i);
+  }
+}
