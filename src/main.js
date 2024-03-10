0f7efb45d878796623c4cd1a6f802169b30df73e
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-10 18:31:19 +0100

    button onclick basic testing on one element

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // if number has less that 6 digits, add leading zeros
-const randomNumber = (document.getElementById("1").innerHTML = Math.floor(
-  Math.random() * 999999
-)
-  .toString()
-  .padStart(6, "0"));
+const randomNumber = () => {
+  document.getElementById("1").innerHTML = Math.floor(Math.random() * 999999)
+    .toString()
+    .padStart(6, "0");
+};
