f90788c615e8e203d338a58a6be34de5bb474905
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-04-29 19:14:22 +0200

    small boxes maximum 10 with pad 0

--- src/main.js
+++ src/main.js
@@ -9,10 +9,21 @@ function generateRandomNumbers(numberOfElements) {
   removeAndAddAnimateClassTimer('play-button', 2000);
   for (let id = 1; id <= numberOfElements; id++) {
     const digits = id <= 4 ? 6 : 2;
-    let randomNumber = Math.floor(
-      Math.random() * Math.pow(10, digits)
-    ).toString();
-    randomNumber = randomNumber.padStart(digits, '0');
+    let randomNumber;
+
+    if (digits === 2) {
+      randomNumber = Math.floor(Math.random() * 11).toString();
+      randomNumber = randomNumber.padStart(digits, '0');
+    } else {
+      randomNumber = Math.floor(
+        Math.random() * Math.pow(10, digits)
+      ).toString();
+      randomNumber = randomNumber.padStart(digits, '0');
+    }
+
+    if (digits === 2 && randomNumber < 10) {
+      randomNumber = randomNumber.padStart(2, '0');
+    }
 
     elementSetNumber(id, randomNumber);
   }
