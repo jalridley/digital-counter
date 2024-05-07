00096c4af56f98114453e1ea8a09ca2a613460ed
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-05-07 21:26:02 +0200

    emerald to teal gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         'radial-gradient-green': 'radial-gradient(#d9f99d, #84cc16, #65a30d)',
         'radial-gradient-red': 'radial-gradient(#f87171, #dc2626, #b91c1c)',
         'radial-gradient-amber': 'radial-gradient(#fde68a, #f59e0b, #d97706)',
-        'radial-gradient-teal': 'radial-gradient(#a7f3d0,  #2dd4bf, #14b8a6)',
+        'radial-gradient-teal': 'radial-gradient(#d1fae5,  #2dd4bf, #14b8a6)',
       },
     },
   },
